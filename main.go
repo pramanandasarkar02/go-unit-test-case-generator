0f21bc12c21d00b51a32d4bee9d852eef963d2b2
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-04-19 08:34:57 +0600

    filename parsing and output file generation working fine

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func main() {
 	// take flags as cmd line
+
+
+	inputFilePath := flag.String("i", "", "-i <input_file_name>")
+	flag.Parse()
+
+	if *inputFilePath == "" {
+		panic("input file required!")
+	}
+	inputFileName := filepath.Base(*inputFilePath)
+	inputFileParts := strings.Split(inputFileName, ".")
+	if len(inputFileParts) < 2 {
+		log.Fatal("invalid file name format")
+	}
+
+	outputFileName := filepath.Dir(*inputFilePath) + "/" + inputFileParts[0] + "_test." + inputFileParts[1]
+
+	fmt.Println("Output file name:", outputFileName)
+
+	outputFile , err := os.Create(outputFileName)
+	if err != nil{
+		panic(err)
+	}
+
+	outputFile.WriteString("demo text is written \n in that \t\t\tfile")
+
+	
+
+
 
 	fmt.Println("cmd line flags loading...")
 
